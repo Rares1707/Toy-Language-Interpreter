293b85865bb81acda77779f5c69aa13c59757c75
Șuchea Rareș-Andrei <115061254+Rares1707@users.noreply.github.com>
2023-12-29 18:48:31 +0200

    Delete Project files directory

--- Project files/model/myADTs/MyIList.java
+++
@@ -1,9 +0,0 @@
-package model.myADTs;
-
-public interface MyIList<T> {
-    boolean add(T element);
-    void add(int index, T element);
-    void clear();
-    String toString();
-    boolean isEmpty();
-}
--- Project files/model/types/IType.java
+++
@@ -1,9 +0,0 @@
-package model.types;
-
-import model.values.IValue;
-
-public interface IType {
-    IType deepCopy();
-
-    IValue getDefaultValue();
-}
